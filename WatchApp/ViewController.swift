7959182bc06832e185dec192aa321705a4397e49
MacStudent <macstudent@130-iMac39.local>
2019-02-27 08:57:25 -0500

    Initial Commit

---
+++ WatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchApp
+//
+//  Created by MacStudent on 2019-02-27.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
